4a77f7313580c7defc719446f9f08599791f1271
antixz1 <99371704+antixz1@users.noreply.github.com>
2022-04-19 18:30:35 -0500

    Update 2.data.manip.R

--- 2.data.manip.R
+++ 2.data.manip.R
@@ -10,8 +10,9 @@
 data.death <- read.csv(paste(p.data.raw, "stmf.csv", sep = ""), header = TRUE, stringsAsFactors = FALSE)
 data.rank <- read.csv(paste(p.data.raw, 'ranking.csv', sep=''),header = TRUE, stringsAsFactors = FALSE)
 c.names <- read.csv(paste(p.data.raw, 'country_names.csv',sep=''),header = TRUE, stringsAsFactors = FALSE)
+data.obesity <- read.csv(paste(p.data.raw,'ObesityDataSet_raw_and_data_sinthetic.csv',sep=''),header=TRUE,stringAsFactors=FALSE)
 #==============================================================================#
 
 # * 1.2 Housekeeping ----
-
+obesity.numerical <- data.matrix(data.obesity) #Categorical --> Numerical predictors
 #==============================================================================#
